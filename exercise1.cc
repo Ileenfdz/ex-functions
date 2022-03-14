1e689376f3da1ba7b778b5cb99f0fc88ff8e9b6e
Ileenfdz <ileenfdz@gmail.com>
2022-03-14 22:07:01 +0100

    Added succesfully the first exercise

--- exercise1.cc
+++ exercise1.cc
@@ -2,9 +2,19 @@
 
 using namespace std;
 
-int main(){
+void evenOdd(int a){
+    a%2==0? cout<<"Tu numero es par"<<endl:
+    cout<<"Tu numero es impar"<<endl;
+}
 
+int main (void){
+	int a;
 
-    system('PAUSE');
-    return 0;
+	cout<<"Dime un numero :  ";
+	cin>>a;
+
+	evenOdd (a);
+
+	system ("PAUSE");
+	return 0;
 }
